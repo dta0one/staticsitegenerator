543ef6d91825163a4a5a7fc4880a9bad49882efd
dta0one <github@davidtao.net>
2025-04-05 09:05:03 -0700

    ssg Ch2L6

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
+from htmlnode import HTMLNode, ParentNode, LeafNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -30,6 +31,45 @@ class TestTextNode(unittest.TestCase):
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
 
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("Bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "Bold text")
+        self.assertIsNone(html_node.props)
+
+    def test_italic(self):
+        node = TextNode("Italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "Italic text")
+        self.assertIsNone(html_node.props)
+
+    def test_link(self):
+        node = TextNode("Click me", TextType.LINK, url="https://example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click me")
+        self.assertEqual(html_node.props["href"], "https://example.com")
+        self.assertIsNone(html_node.props.get("alt"))  # Ensure no extra props
+
+    def test_image(self):
+        node = TextNode("Description of image", TextType.IMAGE, url="https://image.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")  # IMAGE value should be blank
+        self.assertEqual(html_node.props["src"], "https://image.com")
+        self.assertEqual(html_node.props["alt"], "Description of image")
+
+    def test_invalid_type(self):
+        class RandomNode:
+            pass
 
 if __name__ == "__main__":
     unittest.main()
