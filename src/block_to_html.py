a0c8c87f877fd1ff25eec8e2e6dae539296233e6
dta0one <github@davidtao.net>
2025-06-01 22:31:35 -0700

    SSG CH4L3b

---
+++ src/block_to_html.py
@@ -0,0 +1,11 @@
+from splitblocks import markdown_to_blocks
+
+def markdown_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        pass
+
+
+def markdown_to_blocks(markdown):
+    blocks = markdown_to_blocks(markdown)
+    return blocks
