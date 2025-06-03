8d253bc8cf6f0f4d98869c43be3172a9c1315c7f
dta0one <github@davidtao.net>
2025-06-02 17:48:03 -0700

    SSG CH4L3c

--- src/block_to_html.py
+++ src/block_to_html.py
@@ -1,11 +1,39 @@
+import re
 from splitblocks import markdown_to_blocks
+from blocktype import BlockType, block_to_block_type
+from htmlnode import HTMLNode, ParentNode, LeafNode
 
 def markdown_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
     for block in blocks:
-        pass
+        type = block_to_block_type(block)
+        match type:
+            case BlockType.HEADING:
+                pass
+            case BlockType.CODE:
+                pass
+            case BlockType.QUOTE:
+                block = ParentNode("blockquote", text_to_children(block), None)
+            case BlockType.UNORDERED_LIST:
+                block = ParentNode("ul", list_to_children(block), None)
+            case BlockType.ORDERED_LIST:
+                block = ParentNode("ol", list_to_children(block), None)
+            case BlockType.PARAGRAPH:
+                block = ParentNode("p", text_to_children(block), None)
 
 
-def markdown_to_blocks(markdown):
-    blocks = markdown_to_blocks(markdown)
-    return blocks
+def text_to_children(text):
+    pass
+
+def list_to_children(text):
+    children = []
+    for line in text.split("\n"):
+        if re.match("^- ", line) or re.match("^* ", line) or re.match("^+ ", line):
+            children.append(ParentNode("li",text_to_children(line[2:]), None))
+        elif re.match(rf"^\d+. ", line):
+            match_ordered = re.match(rf"^\d+. ", line)
+            children.append(ParentNode("li", 
+                                       text_to_children(line[match_ordered.span()[1]:]), 
+                                       None))
+    return children
+
