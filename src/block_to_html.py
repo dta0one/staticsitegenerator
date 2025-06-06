ce2a8317cb14e0517ad72afb011826a8691ac912
dta0one <github@davidtao.net>
2025-06-05 18:54:56 -0700

    SSG CH4L4f

--- src/block_to_html.py
+++ src/block_to_html.py
@@ -2,25 +2,33 @@ import re
 from splitblocks import markdown_to_blocks
 from blocktype import BlockType, block_to_block_type
 from htmlnode import HTMLNode, ParentNode, LeafNode
-from textnode import TextNode, text_node_to_html_node
+from textnode import TextNode, text_node_to_html_node, TextType
 
 def markdown_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
+    new_node = []
     for block in blocks:
         type = block_to_block_type(block)
         match type:
             case BlockType.HEADING:
                 pass
             case BlockType.CODE:
-                block = ParentNode("code", text_to_children(block), None)
+                inner_block = [text_node_to_html_node(TextNode(block, TextType.CODE))]
+                block = ParentNode("pre", inner_block, None)
+                new_node.append(block)
             case BlockType.QUOTE:
                 block = ParentNode("blockquote", text_to_children(block), None)
+                new_node.append(block)
             case BlockType.UNORDERED_LIST:
                 block = ParentNode("ul", list_to_children(block), None)
+                new_node.append(block)
             case BlockType.ORDERED_LIST:
                 block = ParentNode("ol", list_to_children(block), None)
+                new_node.append(block)
             case BlockType.PARAGRAPH:
                 block = ParentNode("p", text_to_children(block), None)
+                new_node.append(block)
+    return ParentNode("div", new_node, None)
 
 
 def text_to_children(text):
