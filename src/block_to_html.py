d8d3724dcc0a58156625c182d6e3680c5453648e
dta0one <github@davidtao.net>
2025-06-06 18:20:50 -0700

    SSG CH4L3g

--- src/block_to_html.py
+++ src/block_to_html.py
@@ -3,36 +3,48 @@ from splitblocks import markdown_to_blocks
 from blocktype import BlockType, block_to_block_type
 from htmlnode import HTMLNode, ParentNode, LeafNode
 from textnode import TextNode, text_node_to_html_node, TextType
+from inline_markdown import text_to_textnodes
 
-def markdown_html_node(markdown):
+def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
     new_node = []
     for block in blocks:
         type = block_to_block_type(block)
         match type:
             case BlockType.HEADING:
-                pass
+                matches = re.match(r"^#{1,6}", block)
+                count = len(matches.group(0)) if matches else 0
+                new_block = ParentNode(f"h{count}", text_to_children(block[{count + 1}:]), None)
+                new_node.append(new_block)
             case BlockType.CODE:
-                inner_block = [text_node_to_html_node(TextNode(block, TextType.CODE))]
-                block = ParentNode("pre", inner_block, None)
-                new_node.append(block)
+                split_block = block.splitlines()
+                sliced_block = split_block[1:-1]
+                joined_sliced_block = "\n".join(sliced_block)
+                inner_block = [text_node_to_html_node(TextNode(joined_sliced_block, TextType.CODE))]
+                new_block = ParentNode("pre", inner_block, None)
+                new_node.append(new_block)
             case BlockType.QUOTE:
-                block = ParentNode("blockquote", text_to_children(block), None)
-                new_node.append(block)
+                new_block = ParentNode("blockquote", text_to_children(block), None)
+                new_node.append(new_block)
             case BlockType.UNORDERED_LIST:
-                block = ParentNode("ul", list_to_children(block), None)
-                new_node.append(block)
+                new_block = ParentNode("ul", list_to_children(block), None)
+                new_node.append(new_block)
             case BlockType.ORDERED_LIST:
-                block = ParentNode("ol", list_to_children(block), None)
-                new_node.append(block)
+                new_block = ParentNode("ol", list_to_children(block), None)
+                new_node.append(new_block)
             case BlockType.PARAGRAPH:
-                block = ParentNode("p", text_to_children(block), None)
-                new_node.append(block)
+                new_block = ParentNode("p", text_to_children(block), None)
+                new_node.append(new_block)
     return ParentNode("div", new_node, None)
 
 
 def text_to_children(text):
-    pass
+    nodes = text_to_textnodes(text)
+    children = []
+    for node in nodes:
+        children.append(text_node_to_html_node(node))    
+    return children
+
 
 def list_to_children(text):
     children = []
--- src/blocktype.py
+++ src/blocktype.py
@@ -14,7 +14,7 @@ def block_to_block_type(block):
     lines = block.strip().split('\n')
     if re.match(r"^#{1,6} (.*)", block) and len(lines) == 1:
         return BlockType.HEADING
-    if re.match(r"^`{3}$", lines[0]) and re.match(r"^`{3}$", lines[-1]):
+    if re.match(r"^`{3}$", lines[0]) and re.match(r"^`{3}$", lines[-1]) and len(lines) > 2:
         return BlockType.CODE
     if all(re.match(r"^>", line) for line in lines):
         return BlockType.QUOTE
