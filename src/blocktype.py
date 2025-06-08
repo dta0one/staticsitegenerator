9dc940f29a04cf1a2ac828680588644f21bbcfb7
dta0one <github@davidtao.net>
2025-06-08 00:02:21 -0700

    SSGCh4L3i

--- src/blocktype.py
+++ src/blocktype.py
@@ -12,15 +12,23 @@ class BlockType(Enum):
 
 def block_to_block_type(block):
     lines = block.strip().split('\n')
+    print(f"Block being processed: {block}")
+    print(f"Lines after splitting: {lines}")
     if re.match(r"^#{1,6} (.*)", block) and len(lines) == 1:
+        print("Heading condition is True")
         return BlockType.HEADING
-    if re.match(r"^`{3}$", lines[0]) and re.match(r"^`{3}$", lines[-1]) and len(lines) > 2:
+    if re.match(r"^`{3}$", lines[0].strip()) and re.match(r"^`{3}$", lines[-1].strip()) and len(lines) > 2:
+        print("Code condition is True")
         return BlockType.CODE
     if all(re.match(r"^>", line) for line in lines):
+        print("Quote condition is True")
         return BlockType.QUOTE
     if all(re.match("^- ", line) for line in lines):
+        print("Unordered list condition is True")
         return BlockType.UNORDERED_LIST
     if all(re.match(rf"^{i}\. ", line) for i, line in enumerate(lines, start=1)):
+        print("Ordered list condition is True")        
         return BlockType.ORDERED_LIST
     else:
+        print("Falling through to Paragraph")
         return BlockType.PARAGRAPH
