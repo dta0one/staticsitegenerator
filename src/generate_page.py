b63715ffdb30c4b3c2e90f685205694e4d762bf2
dta0one <github@davidtao.net>
2025-06-11 21:23:29 -0700

    SSG CH5L2b

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,12 +1,18 @@
-
+from block_to_html import markdown_to_html_node
+from htmlnode import HTMLNode
 
 
 def extract_title(markdown):
-    pass
-
-
-
+    #pull h1 header
+    if not h1:
+        raise Exception("No h1 header")
+    return h1
 
 
 def generate_page(from_path, template_path, dest_path):
-    pass
+    print("Generating page from {from_path}, to {dest_path} using {template_path}.")
+    open_from_path = open(from_path)
+    open_template_path = open(template_path)
+    html_string = markdown_to_html_node(open_from_path).to_html()
+    title = extract_title(open_from_path)
+    
