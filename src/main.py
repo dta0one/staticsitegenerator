5de2bfb4321e7d53e331d2166892ba8fe1dcd5cb
dta0one <github@davidtao.net>
2025-06-13 17:52:54 -0700

    SSG CH5L4 Final

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import os, shutil, glob
 
 from copystatic import copy_files_recursive
-from utils import generate_page
+from utils import generate_pages_recursive
 
 
 dir_path_static = "./static"
 dir_path_public = "./public"
-dir_path_template = "./template.html"
-dir_path_content = "./content/index.md"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
     print("Deleting public directory...")
@@ -16,12 +16,14 @@ def main():
 
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
-    generate_page(
-        dir_path_content, 
-        dir_path_template, 
-        os.path.join(dir_path_public, "index.html")
-        )
-
+#    markdown_files = glob.glob("content/**/*.md", recursive=True)
+#
+#    for markdown_file in markdown_files:
+#        html_file = markdown_file.replace("content", "public", 1).replace(".md", ".html")
+#        os.makedirs(os.path.dirname(html_file), exist_ok=True)
+#        generate_page(markdown_file, template_path, html_file)
+        
 
 main()
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-import re, os
+import re, os, glob
 
 from block_to_html import markdown_to_html_node
 from htmlnode import HTMLNode
@@ -12,18 +12,30 @@ def extract_title(markdown):
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path}, to {dest_path} using {template_path}.")
+
     with open(from_path) as file:
         content = file.read()
+
     with open(template_path) as file:
         template = file.read()
+
     processed_content = markdown_to_html_node(content).to_html()    
     processed_title = extract_title(content)
+
     new_page = (template
         .replace("{{ Title }}", processed_title)
         .replace("{{ Content }}", processed_content)
     )
     
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+
     with open(dest_path, "w") as file:
         file.write(new_page)
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    markdown_files = glob.glob(f"{dir_path_content}/**/*.md", recursive=True)
+
+    for markdown_file in markdown_files:
+        html_file = markdown_file.replace(dir_path_content, dest_dir_path, 1).replace(".md", ".html")
+        os.makedirs(os.path.dirname(html_file), exist_ok=True)
+        generate_page(markdown_file, template_path, html_file)
