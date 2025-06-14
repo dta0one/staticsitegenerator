3f2518dab5022a0fc4f2d467093f766b59e7ef3a
dta0one <github@davidtao.net>
2025-06-13 19:53:09 -0700

    SSG CH5L5c

--- src/main.py
+++ src/main.py
@@ -14,11 +14,11 @@ dir_path_content = "./content"
 template_path = "./template.html"
 
 def main():
-    print("Deleting public directory...")
+    print("Deleting docs directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
-    print("Copying static files to public directory...")
+    print("Copying static files to docs directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
     generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
